14c7d9eb7ac1b127bb66ee06de24d1bd228bd147
annnnnne0811 <annnnnneflores2000@yahoo.com>
2025-02-05 07:25:21 +0200

    theme

--- Theming/app/src/main/java/com/example/theming/MainActivity.kt
+++ Theming/app/src/main/java/com/example/theming/MainActivity.kt
@@ -4,44 +4,95 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.theming.ui.theme.ThemingTheme
 
-class MainActivity : ComponentActivity() {
+class MainActivity : ComponentActivity(){
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent {
+        setContent{
             ThemingTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+
+                //Surface using the background colour  from the theme
+                Surface (
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ){
+                    MyApp()
                 }
             }
         }
     }
 }
-
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun MyApp(){
+
+    //Modifier for applying the composable
+    val appModifier = Modifier
+        .fillMaxWidth()
+        .padding(8.dp)
+
+    //Column composable to arrange vertically
+    Column(
+        verticalArrangement = Arrangement.spacedBy(16.dp)
+    ){
+        //Text composable for displaying the title
+        Text(
+            text = "My title",
+            style = MaterialTheme.typography.titleLarge,
+            modifier = appModifier
+        )
+
+
+
+        //OutlinedTextField composable for input
+        OutlinedTextField(
+            value = "",
+            onValueChange = {/*TODO*/},
+            modifier = appModifier
+        )
+
+
+
+        //Button composable for user interaction
+        Button(
+            onClick = {/*TODO*/},
+            modifier = appModifier
+
+        ) {
+            //text composable inside the button
+            Text(text = "Submit")
+
+        }
+
+    }//End vertical Arrangement
+}//End Myapp
+
+
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun DefaultPreview() {
     ThemingTheme {
-        Greeting("Android")
+
+
+        //Calls the main Composable function for the preview
+        MyApp()
     }
 }
